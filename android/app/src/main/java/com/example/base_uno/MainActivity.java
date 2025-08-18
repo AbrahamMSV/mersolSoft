3f60e2cd98cf9fed4e42f7779d85cbc747c92d24
AbrahamMSV <220300m.s.abraham.pen@gmail.com>
2025-08-17 21:53:15 -0600

    primer commit

---
+++ android/app/src/main/java/com/example/base_uno/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.base_uno;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'views/user_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter API Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+      ),
+      home: const UserScreen(),
+    );
+  }
+}
